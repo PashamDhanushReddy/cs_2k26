b0212064ea42fc6adcd1d887d85f943ffd215775
ReddemVarshith <reddemvarshith@gmail.com>
2025-12-29 15:16:24 +0530

    static to dyn2

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register_team, name='register'),
     path('register', views.register_team),
+    path('CS2026/register/', views.register_team),
+    path('CS2026/register.html', views.register_team),
     path('registration-success/', views.registration_success, name='registration_success'),
     path('registration-success', views.registration_success),
+    path('CS2026/registration-success/', views.registration_success),
+    path('CS2026/registration-success.html', views.registration_success),
 ]
